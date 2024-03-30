1ff02237ce57a7f381e4327add6073dc4c55d295
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-03-30 06:07:26 +0100

    created class, added geter, added method print

--- main.js
+++ main.js
@@ -4,3 +4,28 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field) {
+        this._field = field;
+    }
+
+    get field() {
+        return this._field;
+    }
+
+    print() {
+        // соеденяю вложенные массивы
+        const joinedArray = this._field.map(innerArray => innerArray.join(''));
+        // выводим на консоль и соеденяем 2д массив
+        console.log(joinedArray.join(''));
+    }
+}
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░']
+]);
+
+myField.print();
