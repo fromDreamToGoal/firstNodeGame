35189a7e630d7547608730c748d67d2368d3ce9c
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-04-01 17:45:27 +0200

    added method to generate random position for hat

--- main.js
+++ main.js
@@ -7,25 +7,36 @@ const pathCharacter = '*';
 
 class Field {
     constructor(field) {
-        this._field = field;
+        this.field = field;
+        this.rows = field.length;
+        this.cols = field[0].length;
+        this.playerRow = 0;
+        this.playerCol = 0;
     }
 
-    get field() {
-        return this._field;
-    }
+    
 
     print() {
         // соеденяю вложенные массивы
-        const joinedArray = this._field.map(innerArray => innerArray.join(''));
+        const joinedArray = this.field.map(innerArray => innerArray.join(''));
         // выводим на консоль и соеденяем 2д массив
         console.log(joinedArray.join(''));
     }
+    // Ставит шляпу в произвольное положение на поле
+    generateHat() {
+        const randomRow = Math.floor(Math.random() * this.rows);
+        const randomCol = Math.floor(Math.random() * this.cols);
+        this.field[randomRow][randomCol] = '^';
+    }
 }
+
+
 
 const myField = new Field([
     ['*', '░', 'O'],
     ['░', 'O', '░'],
-    ['░', '^', '░']
+    ['░', '░', '░']
 ]);
 
+myField.generateHat();
 myField.print();
